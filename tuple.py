c95e24a0497d2b14e4c3fe972eabe8d987b50d2f
Ayaan-Singh <asthana.divya@gmail.com>
2024-12-09 18:59:00 +0530

    hi

---
+++ tuple.py
@@ -0,0 +1,14 @@
+
+tuplex = ("tuple",False,2,3.5)
+print(tuplex)
+tuplex = (4,3,23,4)
+print(tuplex)
+tuplex = tuplex + (9,)
+print (tuplex)
+tuple = (50,50,53,0,32,5)
+print (tuple)
+tuplex =(240,4,35,3,5,2,4)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
